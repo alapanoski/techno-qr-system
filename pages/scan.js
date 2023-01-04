7d705cddc36f7f9860ab635a407c92c77b22634a
Aldrin Jenson <aldrinjenson@gmail.com>
2023-01-04 18:03:35 +0530

    Add base scanner

---
+++ pages/scan.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const Scan = () => {
+  return (
+    <>
+      <Head>
+        <title>QR Scanner </title>
+        <meta name='description' content='QR Code scanner' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div>
+        <h1>QR Scanner</h1>
+      </div>
+    </>
+  );
+};
+
+export default Scan;
