80c3d852e231e39e14868733ee84ed2be554d94b
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-01-06 21:20:54 +0530

    Add QR reading flow

--- src/pages/checkin.js
+++ src/pages/checkin.js
@@ -1,70 +1,100 @@
-import { Footer, Navbar, Scanner } from "../components";
-import styles from "../styles/Checkin.module.css";
-import Box from "@mui/material/Box";
-import Stepper from "@mui/material/Stepper";
-import Step from "@mui/material/Step";
-import StepButton from "@mui/material/StepButton";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import React from "react";
+import { Footer, Navbar, Scanner } from "../components"
+import styles from "../styles/Checkin.module.css"
+import Box from "@mui/material/Box"
+import Stepper from "@mui/material/Stepper"
+import Step from "@mui/material/Step"
+import StepButton from "@mui/material/StepButton"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import React from "react"
+import { supabase } from "../utils/supabaseClient"
 
-const steps = [
-  "Verify Payment Details",
-  "Assign User ID",
-  "Confirm User",
-];
+const steps = ["Verify Payment Details", "Assign User ID", "Confirm User"]
 const Checkin = () => {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [completed, setCompleted] = React.useState({});
+  const [activeStep, setActiveStep] = React.useState(0)
+  const [completed, setCompleted] = React.useState({})
+
+  const [qrResult, setQrResult] = React.useState("")
+  const [paymentId, setPaymentId] = React.useState(null)
+  const [userId, setUserId] = React.useState(null)
 
   const totalSteps = () => {
-    return steps.length;
-  };
+    return steps.length
+  }
 
   const completedSteps = () => {
-    return Object.keys(completed).length;
-  };
+    return Object.keys(completed).length
+  }
 
   const isLastStep = () => {
-    return activeStep === totalSteps() - 1;
-  };
+    return activeStep === totalSteps() - 1
+  }
 
   const allStepsCompleted = () => {
-    return completedSteps() === totalSteps();
-  };
+    return completedSteps() === totalSteps()
+  }
 
-  const handleNext = () => {
+  const handleNext = async () => {
     const newActiveStep =
       isLastStep() && !allStepsCompleted()
         ? // It's the last step, but not all steps have been completed,
           // find the first step that has been completed
           steps.findIndex((step, i) => !(i in completed))
-        : activeStep + 1;
-    setActiveStep(newActiveStep);
-  };
+        : activeStep + 1
+    setActiveStep(newActiveStep)
+
+    try {
+      if (activeStep === 0) {
+        console.log("Step 1")
+        if (qrResult != "") {
+          // Todo: Add validation
+          setPaymentId(qrResult)
+          setQrResult("")
+        }
+      } else if (activeStep === 1) {
+        console.log("Step 2")
+
+        // Todo: Add validation
+
+        // Getting user id from QR code
+        setUserId(qrResult.split("/")[3])
+      } else if (activeStep === 2) {
+        console.log("Step 3")
+
+        // Todo: Add validation
+
+        const { data, error } = await supabase.from("users").insert({
+          name: "James Bond",
+          payment_id: paymentId,
+          user_id: userId,
+        })
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+    setActiveStep((prevActiveStep) => prevActiveStep - 1)
+  }
 
   const handleStep = (step) => () => {
-    setActiveStep(step);
-  };
+    setActiveStep(step)
+  }
 
   const handleComplete = () => {
-    const newCompleted = completed;
-    newCompleted[activeStep] = true;
-    setCompleted(newCompleted);
-    handleNext();
-  };
+    const newCompleted = completed
+    newCompleted[activeStep] = true
+    setCompleted(newCompleted)
+    handleNext()
+  }
 
   const handleReset = () => {
-    setActiveStep(0);
-    setCompleted({});
-  };
+    setActiveStep(0)
+    setCompleted({})
+  }
   return (
     <>
-      
       <Box sx={{ width: "100%" }}>
         <Stepper nonLinear activeStep={activeStep}>
           {steps.map((label, index) => (
@@ -88,7 +118,7 @@ const Checkin = () => {
             </React.Fragment>
           ) : (
             <React.Fragment>
-              <Scanner/>
+              <Scanner setQrResult={setQrResult} />
               <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
                 <Button
                   color="inherit"
@@ -123,7 +153,7 @@ const Checkin = () => {
         </div>
       </Box>
     </>
-  );
-};
+  )
+}
 
-export default Checkin;
+export default Checkin
