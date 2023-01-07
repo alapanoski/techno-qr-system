bc749d648d667567fab4fc97be850891b3a47b04
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-01-07 17:52:38 +0530

    Update user pages

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,27 +1,34 @@
+import Image from "next/image";
+import profilePic from "../../assets/temp_profile.jpeg";
+import linkedinImage from "../../assets/linkedin.png";
+import githubImage from "../../assets/github.png";
+import emailImage from "../../assets/email.png";
+
 export default function User({ user }) {
   return (
-    <div className="w-96 p-4 flex flex-col gap-4 overflow-y-scroll">
-      <p className="text-3xl font-bold tracking-wide">Basic Details</p>
-      <div className="flex flex-col gap-2 text-xl">
-        <p>
-          User Id: <strong>{user.user_id}</strong>
-        </p>
-        <p>
-          RazorPay Id: <strong>{user.payment_id}</strong>
-        </p>
-        <p>
-          Name: <strong>{user.name}</strong>
-        </p>
-      </div>
-      <div className="flex flex-col gap-4">
-        <p className="text-2xl font-bold tracking-wide">Food</p>
-        <div className="flex flex-col justify-evenly gap-2">
-          <p>Breakfast ✅</p>
-          <p>Lunch ❌</p>
+    <div className="w-full p-8 flex flex-col items-center gap-4">
+      <Image
+        alt="Profile Image"
+        src={profilePic}
+        width="200"
+        height="200"
+        className="w-[200px] h-[200px] object-cover rounded-full border-4 border-black"
+      ></Image>
+      <div className="flex flex-col justify-evenly gap-8">
+        <p className="text-4xl">{user.name}</p>
+
+        <div className="flex flex-row items-center gap-4">
+          <a href={user.linkedin}>
+            <Image src={linkedinImage} alt="LinkedIn" width="53" height="53" />
+          </a>
+          <a href={user.email}>
+            <Image src={emailImage} alt="Email" width="55" height="55" />
+          </a>
+          <a href={user.github}>
+            <Image src={githubImage} alt="GitHub" width="40" height="40" />
+          </a>
         </div>
-      </div>
-      <div className="flex flex-row gap-4 text-2xl">
-        <p className="font-bold tracking-wide">Points 100</p>
+        <p className="text-3xl font-bold text-center">Points: 100</p>
       </div>
     </div>
   );
--- src/pages/users/[user_id].js
+++ src/pages/users/[user_id].js
@@ -10,68 +10,14 @@ const Post = () => {
     user_id: user_id,
     payment_id: "00007",
     name: "James Bond",
+    linkedin:"https://www.linkedin.com",
+    email:"email@email.com",
+    github:"https://www.github.com",
+    profile_image:"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
   };
 
-  const users = [
-    {
-      user_id: "00001",
-    },
-    {
-      user_id: "00002",
-    },
-    {
-      user_id: "00003",
-    },
-    {
-      user_id: "00004",
-    },
-    {
-      user_id: "00005",
-    },
-    {
-      user_id: "00006",
-    },
-    {
-      user_id: "00007",
-    },
-    {
-      user_id: "00008",
-    },
-    {
-      user_id: "00009",
-    },
-    {
-      user_id: "00010",
-    },
-    {
-      user_id: "00011",
-    },
-    {
-      user_id: "00012",
-    },
-    {
-      user_id: "00013",
-    },
-
-    {
-      user_id: "00014",
-    },
-  ];
-
   return (
-    <div className="container mx-auto max-h-screen flex flex-row justify-center gap-2">
-      <div className="md:w-72 p-4 overflow-y-scroll">
-        {users.map((user) => {
-          return (
-            <div
-              key={user.user_id}
-              className="h-10 flex flex-row justify-start items-center"
-            >
-              <p>User</p>
-            </div>
-          );
-        })}
-      </div>
+    <div className="container mx-auto max-h-screen flex justify-center gap-2">
       <User user={user} />
     </div>
   );
