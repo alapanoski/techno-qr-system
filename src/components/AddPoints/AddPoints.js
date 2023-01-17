c6fa4b6291ab54065351cbc40470b9499ddc5a06
jaison080 <jaisondennis080@gmail.com>
2023-01-17 22:25:45 +0530

    Score COmpleted

--- src/components/AddPoints/AddPoints.js
+++ src/components/AddPoints/AddPoints.js
@@ -1,13 +1,22 @@
-import { Box, Button, Step, StepButton, Stepper, Typography } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Button,
+  Step,
+  StepButton,
+  Stepper,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 import supabaseClient from "../../utils/supabaseClient";
 import Scanner from "../Scanner/Scanner";
+import styles from "./AddPoints.module.css";
 
 const AddPoints = () => {
   const [queryId, setQueryId] = React.useState("");
   const [currentUser, setCurrentUser] = React.useState(null);
   const [points, setPoints] = React.useState(0);
   const [activeStep, setActiveStep] = React.useState(0);
+  const [userId, setUserId] = React.useState("");
   const [completed, setCompleted] = React.useState({});
   const [foodData, setFoodData] = useState([]);
   const steps = ["Verify User ID", "Add Score"];
@@ -16,14 +25,7 @@ const AddPoints = () => {
   const totalSteps = () => {
     return steps.length;
   };
-  async function getFoodMenu() {
-    const { data, error } = await supabaseClient.from("food_menu").select();
-    setFoodData(data);
-    setLoading(false);
-  }
-  useEffect(() => {
-    getFoodMenu();
-  }, []);
+
   const completedSteps = () => {
     return Object.keys(completed).length;
   };
@@ -58,55 +60,30 @@ const AddPoints = () => {
         alert("Please enter a valid user id");
       } else {
         const { data, error } = await supabaseClient
-          .from("food_log")
+          .from("users")
           .select()
-          .eq("techno_id", userId)
-          .eq("food_id", foodData.find((food) => food.id === foodTab).id);
-        setFoodEaten(data);
-        const newCompleted = completed;
-        newCompleted[activeStep] = true;
-        setCompleted(newCompleted);
-        handleNext();
+          .eq("techno_id", userId);
+        if (error) console.log(error);
+        setCurrentUser(data[0]);
       }
     }
     if (activeStep === 1) {
-      if (foodEaten&&foodEaten.length > 0) {
-        alert("User has already checked in for this food");
-        setUserId("");
-        handleReset();
-        setFoodTab(0);
-        return;
-      } else {
-        const { error } = await supabaseClient.from("food_log").insert({
-          techno_id: userId,
-          food_id: foodData.find((food) => food.id === foodTab).id,
-        });
-        if (error) {
-          console.error(error);
-        }
-      }
-
-      const newCompleted = completed;
-      newCompleted[activeStep] = true;
-      setCompleted(newCompleted);
-      handleNext();
-      setUserId("");
-      setFoodTab(0);
+      const { data, error } = await supabaseClient
+        .from("users")
+        .update(currentUser)
+        .eq("techno_id", currentUser.techno_id);
+      console.log(error);
     }
+    const newCompleted = completed;
+    newCompleted[activeStep] = true;
+    setCompleted(newCompleted);
+    handleNext();
   };
 
   const handleReset = () => {
     setActiveStep(0);
     setCompleted({});
   };
-  async function fetchUser() {
-    const { data, error } = await supabaseClient
-      .from("users")
-      .select()
-      .eq("techno_id", queryId);
-    if (error) console.log(error);
-    setCurrentUser(data[0]);
-  }
 
   async function addPoints() {
     console.log(points);
@@ -125,130 +102,121 @@ const AddPoints = () => {
     console.log(currentUser);
   }
 
-  async function updatePoints() {
-    const { data, error } = await supabaseClient
-      .from("users")
-      .update(currentUser)
-      .eq("techno_id", currentUser.techno_id);
-    console.log(error);
-  }
 
   return (
     <>
-      {/* <input
-        type="text"
-        name="techno_id"
-        placeholder="techno id"
-        onChange={(e) => {
-          setQueryId(e.target.value);
-        }}
-      />
-      <button onClick={fetchUser}>Fetch</button>
-      {currentUser && (
-        <div>
-          <p>{currentUser.name}</p>
-          <p>{currentUser.points}</p>
-          <input
-            type="number"
-            name="points"
-            placeholder="points"
-            onChange={(e) => {
-              setPoints(e.target.value);
-            }}
-          />
-          <button onClick={addPoints}>Add</button>
-          <button onClick={reducePoints}>Reduce</button>
-          <button onClick={updatePoints}>Update</button>
-        </div>
-      )} */}
-      
       <div className={styles.food_container}>
-      <div className={styles.food_heading}>Points</div>
-      <Box className={styles.box}>
-            <Stepper nonLinear activeStep={activeStep}>
-              {steps.map((label, index) => (
-                <Step key={label} completed={completed[index]}>
-                  <StepButton
-                    color="inherit"
-                    onClick={
-                      activeStep === 0
-                        ? handleComplete
-                        : activeStep === 1
-                        ? handleComplete
-                        : activeStep === 2
-                        ? handleComplete
-                        : handleStep(index)
-                    }
-                  >
-                    {label}
-                  </StepButton>
-                </Step>
-              ))}
-            </Stepper>
-            <div>
-              {allStepsCompleted() ? (
-                <React.Fragment>
-                  <Typography sx={{ mt: 2, mb: 1 }}>
-                    All steps completed - you&apos;re finished
-                  </Typography>
-                  <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-                    <Box sx={{ flex: "1 1 auto" }} />
-                    <Button onClick={handleReset}>Reset</Button>
-                  </Box>
-                </React.Fragment>
-              ) : (
-                <React.Fragment>
-                  {activeStep === 0 ? (
-                    <Scanner
-                      setUserId={setUserId}
-                      qr_pay={false}
-                      userId={userId}
-                    />
-                  ) : activeStep === 1 ? (
-                    
-                      <div className={styles.confirm}>
-                        <div>
-                          Techno ID: <b>{userId}</b>
-                        </div>
-                        <div>
-                          Points: <b>10</b>
+        <div className={styles.food_heading}>Points</div>
+        <Box className={styles.box}>
+          <Stepper nonLinear activeStep={activeStep}>
+            {steps.map((label, index) => (
+              <Step key={label} completed={completed[index]}>
+                <StepButton
+                  color="inherit"
+                  onClick={
+                    activeStep === 0
+                      ? handleComplete
+                      : activeStep === 1
+                      ? handleComplete
+                      : activeStep === 2
+                      ? handleComplete
+                      : handleStep(index)
+                  }
+                >
+                  {label}
+                </StepButton>
+              </Step>
+            ))}
+          </Stepper>
+          <div>
+            {allStepsCompleted() ? (
+              <React.Fragment>
+                <Typography sx={{ mt: 2, mb: 1 }}>
+                  All steps completed - you&apos;re finished
+                </Typography>
+                <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                  <Box sx={{ flex: "1 1 auto" }} />
+                  <Button onClick={handleReset}>Reset</Button>
+                </Box>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                {activeStep === 0 ? (
+                  <Scanner
+                    setUserId={setUserId}
+                    qr_pay={false}
+                    userId={userId}
+                  />
+                ) : activeStep === 1 ? (
+                  <div className={styles.confirm}>
+                    <div>
+                      Techno ID: <b>{userId}</b>
+                    </div>
+                    <div>
+                      Points: <b>{currentUser?.points}</b>
+                    </div>
+                    {currentUser && (
+                      <div className={styles.text_input}>
+                        <input
+                          type="number"
+                          name="points"
+                          placeholder="Enter Points"
+                          onChange={(e) => {
+                            setPoints(e.target.value);
+                          }}
+                        />
+                        <br />
+                        <div className={styles.buttons}>
+                          <button
+                            onClick={addPoints}
+                            className={styles.check_in_btn}
+                          >
+                            Add
+                          </button>
+                          <button
+                            onClick={reducePoints}
+                            className={styles.check_in_btn_1}
+                          >
+                            Reduce
+                          </button>
                         </div>
                       </div>
-                    
-                  ) : (
-                    <div>Something went wrong</div>
-                  )}
-                  <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-                    <Button
-                      color="inherit"
-                      disabled={activeStep === 0}
-                      onClick={handleBack}
-                      sx={{ mr: 1 }}
-                    >
-                      Back
-                    </Button>
-                    <Box sx={{ flex: "1 1 auto" }} />
-                    {activeStep !== steps.length &&
-                      (completed[activeStep] ? (
-                        <Typography
-                          variant="caption"
-                          sx={{ display: "inline-block" }}
-                        >
-                          Step {activeStep + 1} already completed
-                        </Typography>
-                      ) : (
-                        <div className={styles.button} onClick={handleComplete}>
-                          {completedSteps() === totalSteps() - 1
-                            ? "Finish"
-                            : "Complete Step"}
-                        </div>
-                      ))}
-                  </Box>
-                </React.Fragment>
-              )}
-            </div>
-          </Box>
-    </div>
+                    )}
+                  </div>
+                ) : (
+                  <div>Something went wrong</div>
+                )}
+                <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                  <Button
+                    color="inherit"
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    sx={{ mr: 1 }}
+                  >
+                    Back
+                  </Button>
+                  <Box sx={{ flex: "1 1 auto" }} />
+                  {activeStep !== steps.length &&
+                    (completed[activeStep] ? (
+                      <Typography
+                        variant="caption"
+                        sx={{ display: "inline-block" }}
+                      >
+                        Step {activeStep + 1} already completed
+                      </Typography>
+                    ) : (
+                      <div className={styles.button} onClick={handleComplete}>
+                        {completedSteps() === totalSteps() - 1
+                          ? "Finish"
+                          : "Complete Step"}
+                      </div>
+                    ))}
+                </Box>
+              </React.Fragment>
+            )}
+          </div>
+        </Box>
+      </div>
     </>
   );
 };
