c197554929fdf254aea1262b27d231f18d2e183f
jaison080 <jaisondennis080@gmail.com>
2023-01-17 13:31:48 +0530

    Completed Check In feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,50 @@
-import { useState, useEffect, useContext } from "react";
-import Hero from "../components/Hero/Hero";
-import Dashboard from "./dashboard";
-import Head from "next/head";
-import supabaseClient from "../utils/supabaseClient";
+import { useContext, useEffect } from "react";
+import styles from "../styles/Home.module.css";
+import logo from "../assets/logo.png";
 import { UserContext } from "../context/userContext";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import supabaseClient from "../utils/supabaseClient";
 
 export default function Home() {
   const { user, setUser } = useContext(UserContext);
+  async function getUser() {
+    const {
+      data: { user },
+    } = await supabaseClient.auth.getUser();
+
+    console.log(user);
+    setUser(user);
+    if (user) {
+      router.push("/dashboard");
+    }
+  }
+  useEffect(() => {
+    getUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  async function signInWithGoogle() {
+    const { data, error } = await supabaseClient.auth.signInWithOAuth({
+      provider: "google",
+    });
+    console.log(data);
+  }
+
   const router = useRouter();
   if (user) {
     router.push("/dashboard");
   }
   return (
     <>
-      <Head>
-        <title>Techno QR System</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <div className="container" style={{ padding: "0px 0 0px 0" }}>
-        {/* {!user ? <Hero /> : <Dashboard />} */}
-        <Hero />
+      <div className={styles.login_container}>
+        <Image src={logo} alt="" width={400} height={400} />
+        <div
+          className={styles.login_button}
+          variant="contained"
+          onClick={signInWithGoogle}
+        >
+          Login with Google
+        </div>
       </div>
     </>
   );
