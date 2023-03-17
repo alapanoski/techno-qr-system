854cbaca6ce05e13b002f91a31f45d992c3960b4
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-03-17 15:04:59 +0530

    More details about user

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,9 @@ import { Autocomplete, TextField } from "@mui/material";
 function Home() {
   const [name, setName] = React.useState("");
   const [users, setUsers] = React.useState([]);
-  const [id, setId] = React.useState();
+  const [currentUser, setCurrentUser] = React.useState({});
+ // const [currentUserFood, setCurrentUserFood] = React.useState([]);
+  const [foodMenu, setFoodMenu] = React.useState([]);
   const [loading1, setLoading1] = React.useState(false);
 
   async function getUsers() {
@@ -23,9 +25,17 @@ function Home() {
   async function getID(value) {
     setLoading1(true);
     if (value?.techno_id) {
-      setId(value.techno_id);
+      users.forEach(async (user) => {
+        if (user.name === value.name) {
+          setCurrentUser(user);
+          // let temp = await SupabaseClient.from("food_log").select("*").eq("techno_id", user.techno_id).join;
+          // setCurrentUserFood(temp.data);
+         const temp = await SupabaseClient.from("food_menu").select("*");
+         setFoodMenu(temp.data);
+        }
+      })
     } else {
-      setId("Not Found");
+      setCurrentUser("Not Found");
     }
 
     setName("");
@@ -92,12 +102,94 @@ function Home() {
           />
         </div>
 
-        {id && (
-          <div className={styles.login_button}>
-            {loading1 ? <ClipLoader /> : id && id}
-          </div>
-        )}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            gap: "1rem",
+          }}
+        >
+          {currentUser === "Not Found" ? (
+            <p>Not Checked In</p>
+          ) : (
+            <>
+              {/* <p>
+                DB id: <strong>{currentUser.id}</strong>
+              </p> */}
+              <p>
+                Name: <strong>{currentUser.name}</strong>
+              </p>
+              <p>
+                Team: <strong>{currentUser.designation}</strong>
+              </p>
+              <p>
+                Techno Id: <strong>{currentUser.techno_id}</strong>
+              </p>
+              <p>
+                Points: <strong>{currentUser.points}</strong>
+              </p>
+              {/* <p>{JSON.stringify(currentUserFood)}</p> */}
+              <div style={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}>
+                {/* {foodMenu.map((food, index) => {
+                    return (
+                      <div
+                        key={index}
+                        style={{
+                          width: "100%",
+                          display: "flex",
+                          flexDirection: "row",
+                          alignItems: "center",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <p>{food.name}</p>
+                        <p>{food.time}</p>
+                      </div>
+                    );
+                })} */}
+              </div>
+              {/* <p>{currentUser.email}</p>
+              <p>{currentUser.phone}</p>
+              <p>{currentUser.github}</p>
+              <p>{currentUser.devfolio}</p>
+              <p>{currentUser.linkedin}</p>
+              <p>{currentUser.organization}</p>
+              <p>{currentUser.grad_year}</p> */}
+            </>
+          )}
+        </div>
       </div>
+
+      {/* <div style={{ width: "100%" }}>
+        {users.map((user, index) => {
+          return (
+            <div
+              key={index}
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                width: "100%",
+                height: "50px",
+                border: "1px solid gray",
+                padding: "0 1rem",
+              }}
+            >
+              <p>{user.name}</p>
+              <p>{user.techno_id}</p>
+            </div>
+          );
+        })}
+      </div> */}
     </>
   );
 }
