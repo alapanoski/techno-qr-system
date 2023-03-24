85bc91134f43e0bb4901e21f20990d89a010c75d
NzM <nazimfilzer@gmail.com>
2023-03-24 18:04:41 +0530

    Margin and Number on Band

--- src/pages/api/generateQR.js
+++ src/pages/api/generateQR.js
@@ -3,10 +3,10 @@ const Jimp = require("jimp");
 
 export default async function generateQr(req, res) {
     try {
-        const logo = "src/assets/qrLogo_50.jpg";
+        const logo = "src/assets/qrLogo_10.jpg";
         const backgroundImage = "src/assets/tyvek.png"; // add path to background image
         // Enter number of QR codes to generate
-        for (let i = 101; i <= 102; i++) {
+        for (let i = 101; i <= 600; i++) {
             const string = "http://users.technopreneur.co.in/33/T" + i;
             const filePath = 'qrCodes/T' + i + '.png';
             const name = 'T' + i;
@@ -16,34 +16,42 @@ export default async function generateQr(req, res) {
                     dark: '#000000',
                     light: '#FFFFFF',
                 },
-                scale: 2.3,
-                margin: 2.3
+                scale: 2.1,
+                margin: 1.5,
+                errorCorrectionLevel: 'H',
             });
 
             const qrImage = await Jimp.read(filePath);
-            const attachment = await Jimp.read(logo);
+            // const attachment = await Jimp.read(logo);
 
             // Resize the attachment image to fit in the middle of the QR code
-            attachment.resize(qrImage.bitmap.width / 3.9, qrImage.bitmap.height / 3.9); 
-            // attachment.resize(qrImage.bitmap.width / 4.1, qrImage.bitmap.height / 4.1); //4.1
-            qrImage.composite(attachment, qrImage.bitmap.width / 2.6, qrImage.bitmap.height / 2.6);
+            // attachment.resize(qrImage.bitmap.width / 4.1, qrImage.bitmap.height / 4.1); 
+            // qrImage.composite(attachment, qrImage.bitmap.width / 2.6, qrImage.bitmap.height / 2.6);
 
-            const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-            // Font, X coordinate, Y coordinate for the String
-            qrImage.print(font, 195, qrImage.bitmap.height - 33, name);
 
-            const background = await Jimp.read(backgroundImage); // load the background image
+            const background = await Jimp.read(backgroundImage);
             // center the qrImage with logo over the background image
             const x = (background.bitmap.width - qrImage.bitmap.width) / 2;
             const y = (background.bitmap.height - qrImage.bitmap.height) / 2;
-            background.composite(qrImage, x-103, y);
+            background.composite(qrImage, x - 98, y);
+
+            const font = await Jimp.loadFont(Jimp.FONT_SANS_16_WHITE);
+            // create a new Jimp image with the text rotated and transparent background
+            const rotatedText = new Jimp(55, 120, 0x00000000);
+            rotatedText.print(font, 0, 0, name);
+            rotatedText.rotate(-90);
+
+            // composite the rotated text onto the background image
+            const textX = 210;
+            const textY = background.bitmap.height - 63;
+            background.composite(rotatedText, textX, textY);
 
             await background.writeAsync(filePath);
         }
 
         res.status(200).json({ message: "QR codes with images generated" });
     } catch (error) {
-        //console.log(error);
+        console.log(error);
         res.status(500).json({ error });
     }
 }
