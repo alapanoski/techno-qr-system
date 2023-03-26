6db384ddfcd9a1cce4e28ba16933e2a0b1daf292
Aldrin Jenson <aldrinjenson@gmail.com>
2023-03-26 09:19:00 +0530

    add phone + email + total count in admin dashboard

--- src/pages/events/[id].js
+++ src/pages/events/[id].js
@@ -15,6 +15,8 @@ function CheckIn() {
   async function getUsers() {
     setLoading(true);
     const { data } = await SupabaseClient.from("users").select("*");
+    console.log(data);
+
     setUsers(data);
     setLoading(false);
   }
@@ -22,11 +24,11 @@ function CheckIn() {
   async function getRegisterList() {
     setLoading(true);
 
-    if (id){      
+    if (id) {
       const { data, error } = await SupabaseClient.from("register")
-      .select("*, users(*)")
-      .eq("event_id", parseInt(id));
-      console.log(id, data)
+        .select("*, users(*)")
+        .eq("event_id", parseInt(id));
+      // console.log(id, data);
       setRegisterList(data);
       let count = 0;
       data?.forEach((user) => {
@@ -34,7 +36,7 @@ function CheckIn() {
           count++;
         }
       });
-      setNoOfCheckedIn(count)
+      setNoOfCheckedIn(count);
       setLoading(false);
     }
   }
@@ -43,43 +45,42 @@ function CheckIn() {
     getRegisterList();
   }, [id]);
 
-  const rows = registerList?.length ?  registerList.map((user) => ({
-    id: user.id,
-    col1: user?.band_id,
-    col2: user.users.name,
-    col3: user.users.phone,
-    col4: user.users.email,
-    col5: user.check_in_time
-      ? new Date(user?.check_in_time).toLocaleDateString() +
-        ", " +
-        new Date(user?.check_in_time).toLocaleString("en-US", {
-          hour: "numeric",
-          minute: "numeric",
-          hour12: true,
-        })
-      : "Not Checked In",
-    col6: user.users.phone,
-
-  })) : [];
-  
-  
+  const rows = registerList?.length
+    ? registerList.map((user) => ({
+        id: user.id,
+        col1: user?.band_id,
+        col2: user.users.name,
+        col3: user.users.phone,
+        col4: user.users.email,
+        col5: user.check_in_time
+          ? new Date(user?.check_in_time).toLocaleDateString() +
+            ", " +
+            new Date(user?.check_in_time).toLocaleString("en-US", {
+              hour: "numeric",
+              minute: "numeric",
+              hour12: true,
+            })
+          : "Not Checked In",
+        col6: user.users?.category,
+      }))
+    : [];
 
   const columns = [
     { field: "id", headerName: "ID", width: 70 },
     { field: "col1", headerName: "Band ID", width: 140 },
     { field: "col2", headerName: "Name", width: 300 },
-    { field: "col3", headerName: "Phone", width: 400 },
-    { field: "col4", headerName: "Email", width: 400 },
+    { field: "col3", headerName: "Phone", width: 200 },
+    { field: "col4", headerName: "Email", width: 300 },
     {
       field: "col5",
       headerName: "Check In Time",
-      width: 300,
+      width: 200,
     },
     {
       field: "col6",
-      headerName: "Phone",
-      width: 300,
-    }
+      headerName: "Category",
+      width: 200,
+    },
   ];
   if (loading) {
     return <Loader />;
@@ -109,6 +110,8 @@ function CheckIn() {
             textAlign: "center",
           }}
         >
+          Total: <strong>{users?.length}</strong>
+          <br />
           Checked In: <strong>{noOfCheckedIn}</strong>
         </div>
         {/* <div
